cac465d7732329ef62d1ceb1d37cabf7118779b5
joshGilsatrp <jgilstrap23@gmail.com>
2023-07-16 07:32:19 -0700

    Add project files.

---
+++ MathGenerator/Problem.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MathGenerator
+{
+    public class Problem
+    {
+        static int currentDifficulty;
+        static char[] operations = { '+', '-', '*', '/', '%' };
+
+        Random random = new Random();
+        int operandLeft;
+        int operandRight;
+        char operation;
+        int answer;
+        string problem;
+
+        public Problem()
+        {
+            MakeProblem();
+        }
+
+        public string GetProblem()
+        {
+            return problem;
+        }
+        
+        public int GetAnswer()
+        {
+            return answer;
+        }
+        
+        private void MakeProblem()
+        {
+            // Find the highest number possible
+            int topEnd = GetTopEnd();
+
+            // Assign left operand
+            operandLeft = MakeOperand(topEnd);
+            
+            // Randomly add "difficulty"
+            if(random.Next() % 5 == 0)
+            {
+                topEnd += 10;
+            }
+            
+            // Choose an operation at random
+            ChooseOperation();
+            
+            // Prevent division by 0
+            operandRight = MakeOperand(topEnd);
+            if (operandRight == 0)
+            {
+                operandRight += random.Next(1, 10);
+            }
+            
+            // Solve problem and save answer
+            SolveProblem();
+            
+            // Build printable problem
+            BuildString();
+        }
+
+        // Intended to simulate difficulty.
+        // Theory: Bigger number = harder problem
+        private int GetTopEnd()
+        {
+            int topEnd = 0;
+            switch (currentDifficulty)
+            {
+                case 0:
+                    topEnd = 10;
+                    break;
+                case 1:
+                    topEnd = 20;
+                    break;
+                case 2:
+                    topEnd = 30;
+                    break;
+                case 3:
+                    topEnd = 50;
+                    break;
+                case 4:
+                    topEnd = 100;
+                    break;
+
+            }
+            return topEnd;
+        }
+        
+        // Randomlly choose an operand
+        private int MakeOperand(int topEnd)
+        {
+            return random.Next(-topEnd, topEnd);
+        }
+
+        // Radnomlly choose an operation
+        private void ChooseOperation()
+        {
+            operation =  operations[random.Next(operations.Length)];
+        }
+
+        // Build printable problem
+        private void BuildString()
+        {
+            problem = operandLeft + " " + operation + " " + operandRight + " = ";
+        }
+
+        // Save answer to problem
+        private void SolveProblem()
+        {
+            switch (operation)
+            {
+                case '+':
+                    answer = operandLeft + operandRight;
+                    break;
+                case '-':
+                    answer = operandLeft - operandRight;
+                    break;
+                case '*':
+                    answer = operandLeft * operandRight;
+                    break;
+                case '/':
+                    answer = operandLeft / operandRight;
+                    break;
+                case '%':
+                    answer = operandLeft % operandRight;
+                    break;
+
+            }
+        }
+    }
+}
---
+++ MathGenerator/Program.cs
@@ -0,0 +1,86 @@
+﻿namespace MathGenerator
+{
+    internal class Program
+    {
+        static int NUM_QUESTIONS = 5;
+        static void Main(string[] args)
+        {
+            Problem problem = new Problem();
+            PrintIntro();
+            int userInput = 0;
+            int currentAnswer;
+            int correctCounter = 0;
+
+            // Game loop
+            for(int count = 0; count < NUM_QUESTIONS; count++)
+            {
+                // Display problem
+                Prompt(problem);
+                // Vallidate input, reference userInput
+                while(!GetUserAnswer(ref userInput))
+                {
+                    Console.SetCursorPosition(Console.WindowWidth / 2 - 7, Console.WindowHeight / 2);
+                    Console.Write("Invalid input!");
+                    Thread.Sleep(1000);
+                    Prompt(problem);
+                }
+
+                currentAnswer = problem.GetAnswer();
+                if(userInput == currentAnswer)
+                {
+                    correctCounter++;
+                }
+
+                problem = new Problem();
+            }
+
+            WriteEndMessage(correctCounter);
+        }
+
+        // Print permanent message centered at the top of the window
+        static void PrintIntro()
+        {
+            string[,] introBlock = {
+                {"----------------------------"},
+                {"Welcome to the Math Trainer!"},
+                {"----------------------------"},
+                {"Exercise your mental math skills."}
+            };
+            for(int count = 0; count < introBlock.GetLength(0); count++)
+            {
+                Console.SetCursorPosition(Console.WindowWidth / 2 - (introBlock[count,0].Length / 2), Console.WindowTop + count);
+                Console.Write(introBlock[count,0]);
+            }
+        }
+
+        // Validate user input, simultaneously save input upon success
+        static bool GetUserAnswer(ref int userAnswer)
+        {
+            bool validInput = false;
+            if (Int32.TryParse(Console.ReadLine(), out userAnswer))
+            {
+                validInput = true;
+            }
+            return validInput;
+        }
+
+        // Clear screen and reprint everything with current problem
+        static void Prompt(Problem problem)
+        {
+            Console.Clear();
+            PrintIntro();
+            Console.SetCursorPosition(Console.WindowWidth / 2 - (problem.GetProblem().Length / 2), Console.WindowTop + 5);
+            Console.Write(problem.GetProblem());
+        }
+
+        // Write out how many correct answers were recorded
+        static void WriteEndMessage(int counter)
+        {
+            Console.Clear();
+            Console.SetCursorPosition(Console.WindowWidth / 2 - 10, Console.WindowHeight / 2);
+            Console.WriteLine("You got " + counter + " out of " + NUM_QUESTIONS);
+            Console.SetCursorPosition(0, Console.WindowHeight - 1);
+            Console.ReadKey();
+        }
+    }
+}
